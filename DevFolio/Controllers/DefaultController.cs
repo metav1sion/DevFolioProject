62d0ac9d7599247305aff09c009bf1c7a2cd1ebc
Metehan Sözen <metehansozen@outlook.com>
2024-02-14 22:10:26 +0300

    Default Controller Oluşturuldu, UI Kısmının temelleri atıldı.

---
+++ DevFolio/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevFolio.Models;
+
+namespace DevFolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+	    private DbDevFolioEntities db = new DbDevFolioEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+			return PartialView();
+		}
+        public PartialViewResult PartialNavbar()
+        {
+	        return PartialView();
+        }
+        public PartialViewResult PartialFeature()
+        {
+	        var values = db.TblFeature.ToList();
+	        return PartialView(values);
+        }
+        public PartialViewResult PartialProfile()
+        {
+	        var values = db.TblProfile.ToList();
+	        return PartialView(values);
+        }
+	}
+}
