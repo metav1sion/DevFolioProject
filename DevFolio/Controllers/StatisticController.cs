0d83851d3a104c285ade0376bcb836dc19d27fb9
Metehan Sözen <metehansozen@outlook.com>
2024-02-18 13:25:24 +0300

    proje içi fotoğraflar eklendi, küçük hatalar düzeltildi

--- DevFolio/Controllers/StatisticController.cs
+++ DevFolio/Controllers/StatisticController.cs
@@ -19,8 +19,15 @@ namespace DevFolio.Controllers
             ViewBag.skillAvgValue = db.TblSkill.Average(x => x.SkillValue);
             ViewBag.lastSkillTitleName = db.LastSkillTitle().FirstOrDefault();
             ViewBag.coreCategoryProjectCount = db.TblProject.Where(x => x.ProjectCategory == 1).Count();
-			ViewBag.isReadTrue = db.GetTrueReadStatusCount();
-            ViewBag.isReadFalse = db.GetFalseReadStatusCount();
+            ViewBag.isReadTrueCount = db.TblContact.Where(x => x.IsRead == true).Count();
+            List<int> MessageCount = new List<int>();
+            var values = db.MessageLastTenDays().ToList();
+            foreach (var item in values)
+            {
+	            MessageCount.Add((int)item.MessageCount);
+            }
+
+            ViewBag.tendaymessage = MessageCount.Sum();
             return View();
         }
     }
